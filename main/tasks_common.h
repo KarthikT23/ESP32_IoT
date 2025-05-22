3fa7163cfb10f8b7cc5936fee0a41c8cbfc97748
Karthik Thejendra <119528503+KarthikT23@users.noreply.github.com>
2025-05-22 23:06:35 +0530

    Update tasks_common.h

--- main/tasks_common.h
+++ main/tasks_common.h
@@ -33,5 +33,10 @@
 #define DHT22_TASK_PRIORITY						5
 #define DHT22_TASK_CORE_ID						1
 
+// SNTP Time sync task
+#define SNTP_TIME_SYNC_TASK_STACK_SIZE			4096
+#define SNTP_TIME_SYNC_TASK_PRIORITY			4
+#define SNTP_TIME_SYNC_TASK_CORE_ID				1
+
 
 #endif /* MAIN_TASKS_COMMON_H_ */
