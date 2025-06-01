65f2fe0bf4b0f53490d3891672b065d16124164a
Karthik Thejendra <119528503+KarthikT23@users.noreply.github.com>
2025-06-01 22:49:44 +0530

    added bmp180 task parameters

--- main/tasks_common.h
+++ main/tasks_common.h
@@ -33,6 +33,11 @@
 #define DHT22_TASK_PRIORITY						5
 #define DHT22_TASK_CORE_ID						1
 
+// BMP180 Sensor task
+#define BMP180_TASK_STACK_SIZE					4096
+#define BMP180_TASK_PRIORITY					5
+#define BMP180_TASK_CORE_ID						1
+
 // SNTP Time sync task
 #define SNTP_TIME_SYNC_TASK_STACK_SIZE			4096
 #define SNTP_TIME_SYNC_TASK_PRIORITY			4
@@ -40,3 +45,4 @@
 
 
 #endif /* MAIN_TASKS_COMMON_H_ */
+
